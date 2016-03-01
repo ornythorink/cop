55a97f6067282c440b7154fe71c60f8c8e05c1c2
Ornythorink <orny2007@gmail.com>
2016-03-01 06:59:00 +0000

    function match against  et sa config

---
+++ src/Cop/CommonBundle/Extension/Doctrine/MatchAgainst.php
@@ -0,0 +1,59 @@
+<?php
+namespace CopCommonBundle\Extension\Doctrine\Query\Mysql;
+
+use Doctrine\ORM\Query\Lexer;
+use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+use Doctrine\ORM\Query\SqlWalker;
+use Doctrine\ORM\Query\Parser;
+
+/**
+ * "MATCH_AGAINST" "(" {StateFieldPathExpression ","}* InParameter {Literal}? ")"
+ */
+class MatchAgainst extends FunctionNode {
+
+    public $columns = array();
+    public $needle;
+    public $mode;
+
+    public function parse(Parser $parser)
+    {
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+
+        do {
+            $this->columns[] = $parser->StateFieldPathExpression();
+            $parser->match(Lexer::T_COMMA);
+        }
+        while ($parser->getLexer()->isNextToken(Lexer::T_IDENTIFIER));
+
+        $this->needle = $parser->InParameter();
+
+        while ($parser->getLexer()->isNextToken(Lexer::T_STRING)) {
+            $this->mode = $parser->Literal();
+        }
+
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+    }
+
+    public function getSql(SqlWalker $sqlWalker)
+    {
+        $haystack = null;
+
+        $first = true;
+        foreach ($this->columns as $column) {
+            $first ? $first = false : $haystack .= ', ';
+            $haystack .= $column->dispatch($sqlWalker);
+        }
+
+        $query = "MATCH(" . $haystack .
+            ") AGAINST (" . $this->needle->dispatch($sqlWalker);
+
+        if($this->mode && $this->mode->value != "") {
+            $query .= " " . trim($this->mode->dispatch($sqlWalker), "'") . ")";
+        } else {
+            $query .= ")";
+        }
+
+        return $query;
+    }
+}

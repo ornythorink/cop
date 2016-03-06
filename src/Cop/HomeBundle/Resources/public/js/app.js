031ee9265d4f56d90ad79960cffaf0db170157c2
Ornythorink <orny2007@gmail.com>
2016-03-06 02:33:43 +0000

    mise en pause provisoire de backbone

--- src/Cop/HomeBundle/Resources/public/js/app.js
+++ src/Cop/HomeBundle/Resources/public/js/app.js
@@ -1,43 +1,38 @@
 (function() {
-
-    var Profile = Backbone.Model.extend();
-
-    var ProfileList = Backbone.Collection.extend({
-        model: Profile,
-        url: 'http://163.172.129.160/app_dev.php/api/products/bottes/db',
-        parse: function(response){
-             var hotels = _.map(response, function (itemArray, index) {
-                return {
-                    name: itemArray[0].name,
-                    id: index + 1};
-            });
-            console.log(hotels);
-            return hotels;
-        }
-    });
-    // thanks to http://stackoverflow.com/questions/17392713/backbone-parse-nested-json
-
-
-    var ProfileView = Backbone.View.extend({
-        el: "#profiles",
-        template: _.template($('#profileTemplate').html()),
-        render: function(eventName) {
-            _.each(this.model.models, function(profile){
-                var profileTemplate = this.template(profile.toJSON());
-                $(this.el).append(profileTemplate);
-            }, this);
-            return this;
-        }
-    });
-
-    var profiles = new ProfileList();
-    var profilesView = new ProfileView({model: profiles});
-    profiles.fetch({
-        success: function() {
-            profilesView.render();
-        }
-    });
-
-
-
+    //
+    //var Product = Backbone.Model.extend();
+    //
+    //var ProductList = Backbone.Collection.extend({
+    //    model: Product,
+    //    url: 'http://163.172.129.160/app_dev.php/api/products/bottes/db',
+    //    parse: function(response){
+    //         var items = _.map(response, function (itemArray, index) {
+    //            return {
+    //                name: itemArray[0].name,
+    //                id: index + 1};
+    //        });
+    //        return items;
+    //    }
+    //});
+    //// thanks to http://stackoverflow.com/questions/17392713/backbone-parse-nested-json
+    //
+    //var ProductView = Backbone.View.extend({
+    //    el: "#products",
+    //    template: _.template($('#productsTemplate').html()),
+    //    render: function(eventName) {
+    //        _.each(this.model.models, function(product){
+    //            var productTemplate = this.template(product.toJSON());
+    //            $(this.el).append(productTemplate);
+    //        }, this);
+    //        return this;
+    //    }
+    //});
+    //
+    //var products = new ProductList();
+    //var productsView = new ProductView({model: products});
+    //products.fetch({
+    //    success: function() {
+    //        productsView.render();
+    //    }
+    //});
 })();

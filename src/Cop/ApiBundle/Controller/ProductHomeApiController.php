e7b580645be27443b2f2e150e3cdc06fc55e9d15
Ornythorink <orny2007@gmail.com>
2016-03-06 11:26:23 +0000

    petit fix pour les  params des urls

--- src/Cop/ApiBundle/Controller/ProductHomeApiController.php
+++ src/Cop/ApiBundle/Controller/ProductHomeApiController.php
@@ -21,7 +21,7 @@ class ProductHomeApiController extends Controller
 
     /**
 
-     * @Route("/home/products/sdc/api/fr/{search}/{page}", name="product_api"
+     * @Route("/home/products/sdc/api/fr/{search}/{page}", 
      * requirements={"page" = "\d+","_locale" = "%app.locales%"},
      * defaults={"page" = "1","_locale" = "fr"})
      */
